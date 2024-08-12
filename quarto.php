76028beb1487b7a7301ef1f061ff78dbccefb15e
MarkRamos98 <marcos.ramos123@live.com>
2024-08-12 19:57:39 -0300

    exercicios

---
+++ quarto.php
@@ -0,0 +1,17 @@
+<?php  
+
+	$data = "12 de Agosto de 2024";
+	$salario = 850.00;
+	$idade = 25;
+	$cargo = 'Estagiário';
+	$resultado = true;
+
+	echo "Data: $data <br />";
+	echo "Salário: $salario <br />";
+	echo "Idade: $idade <br />";
+	echo "Cargo: $cargo <br />";
+
+	if ($resultado) {
+		echo "Resultado é verdadeiro";
+	}
+?>
