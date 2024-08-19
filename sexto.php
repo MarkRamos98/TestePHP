3ef7277576174dcbe3d36ccf73500df10ed8e12e
MarkRamos98 <marcos.ramos123@live.com>
2024-08-19 19:21:33 -0300

    Comando Switch

---
+++ sexto.php
@@ -0,0 +1,23 @@
+<?php 
+	
+	$op = 2;
+
+	switch ($op) {
+		case '1':
+			echo "Voce escolheu Banana";
+			break;
+		case '2':
+			echo "Voce escolheu Laranja";
+			break;
+		case '3':
+			echo "Voce escolheu Abacaxi";
+			break;
+		case '4':
+			echo "Voce escolheu Sair";
+			break;
+		default:
+			// code...
+			break;
+	}
+
+ ?>
