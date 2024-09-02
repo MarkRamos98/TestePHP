e46069f9ba1a720159f40fc3a2baf39b8b8ab9f0
MarkRamos98 <marcos.ramos123@live.com>
2024-09-02 20:27:33 -0300

    Calculadora

--- calculadora.php
+++ calculadora.php
@@ -17,14 +17,52 @@
 			return ($num1 / $num2);
 				}
 
-	echo "Qual operação você deseja?
-	sub
-	sd
-	mult
-	div";
+	echo "Qual operação você deseja?(responda com o número correspondente)<br/>
+	1- sub<br/>
+	2- ad<br/>
+	3- mult<br/>
+	4- div<br/><br/>";
 
-	$op = 
+	$op = "mult";
 
-	
+	echo = "mult<br/><br/>";
+
+	echo "E quais os números a serem calculados?";
+
+	$num1 = 9;
+
+	$num2 = 3;
+
+	if($op == 1) {
+
+		sub($num1,$num2);
+
+		echo $num1 "-" $num2
+
+	} else if($op == 2) {
+
+		ad($num1,$num2);
+
+		echo $num1 "+" $num2
+
+	} else if($op == 3) {
+
+		mult($num1,$num2);
+
+		echo $num1 "*" $num2
+
+	} else if($op == 4) {
+
+		div($num1,$num2);
+
+		echo $num1 "/" $num2
+
+	} else {
+
+		echo "Digite um número válido se não for doer seu dedo"
+
+	}
+
+	echo 
 
  ?>
