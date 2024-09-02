5c2a0764940b18befc7388b0f6094bebaf9c2bc4
MarkRamos98 <marcos.ramos123@live.com>
2024-09-02 19:32:39 -0300

    Calculadora

--- calculadora.php
+++ calculadora.php
@@ -1,11 +1,30 @@
 <?php 
 
-	
-
-	function calc ($num1,$num2){
+	function ad ($num1,$num2){
 			
 			return ($num1 + $num2);
 				}
-	echo "<br /> Resultado:" . calc($num1,$num2);
+	function sub ($num1,$num2){
+			
+			return ($num1 - $num2);
+				}
+	function mult ($num1,$num2){
+			
+			return ($num1 * $num2);
+				}
+	function div ($num1,$num2){
+			
+			return ($num1 / $num2);
+				}
+
+	echo "Qual operação você deseja?
+	sub
+	sd
+	mult
+	div";
+
+	$op = 
+
+	
 
  ?>
