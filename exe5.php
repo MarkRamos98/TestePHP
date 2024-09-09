0f9ea39e0f6fe82f96fd809949a92297ed6c8a01
MarkRamos98 <marcos.ramos123@live.com>
2024-09-09 20:01:42 -0300

    Exercicio

--- exe5.php
+++ exe5.php
@@ -1,5 +1,81 @@
 <?php 
 
-	
+	function ContarIdd($F){
+
+		return$Idd = $F * 1.5;
+
+	}
+
+	function ContarAlt($F){
+
+		return$Alt = ($F * 1.2) + 120;
+
+	}
+
+	function Media($max,$min,$qua) {
+
+		return $R = ($max + $min) / $qua 
+
+	}
+
+	$People = 5;
+	$MaxAlt;
+	$MinAlt;
+	$FMaxAlt;
+	$FMinAlt;
+	$Midd;
+	$Female;
+	$Fidd;
+
+	$F = 0;
+
+	while ($F < $People) {
+		// code...
+
+		If (($F % 2) == 1){
+
+			if (ContarIdd() > $Midd){
+
+				$Midd = ContarIdd($F);
+
+			}
+		
+		} else {
+
+			if (ContarIdd() < $Fidd){
+
+				$Fidd = ContarIdd($F);
+
+			}
+
+			if (ContarAlt() > $FMaxAlt){
+
+				$FMaxAlt = ContarAlt($F);
+
+			} if (ContarAlt() < $FMaxAlt){
+
+				$FMinAlt = ContarAlt($F);
+
+			}
+
+			if (ContarAlt() > $MaxAlt){
+
+				$MaxAlt = ContarAlt($F);
+
+			} if (ContarAlt() < $MaxAlt){
+
+				$MinAlt = ContarAlt($F);
+
+			}
+
+			$Female++;
+
+		}
+
+		$F++;
+
+	}
+
+	Media($FMaxAlt,$FMinAlt,$People)
 
  ?>
